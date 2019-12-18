714320b8d6c6c01d8b7801e917f1021f7d86eba4
Jorge Hernandez <jdaviderb@gmail.com>
2019-12-17 23:08:06 -0500

    enabled lazy loading

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const EngineAddon = require('ember-engines/lib/engine-addon');
 module.exports = EngineAddon.extend({
   name: require('./package').name,
   lazyLoading: {
-    enabled: false
+    enabled: true
   }
 });
